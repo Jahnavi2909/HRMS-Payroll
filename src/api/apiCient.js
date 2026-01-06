7c34afef2626cfd848868f4e984b94753661788d
Jahnavi29 <jahnaviboyina929@gmail.com>
2026-01-06 12:51:10 +0530

    first commit

--- src/api/apiCient.js
+++ src/api/apiCient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 export const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
+  process.env.REACT_APP_API_BASE_URL || "https://d3qs09ype2r7pk.cloudfront.net";
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
