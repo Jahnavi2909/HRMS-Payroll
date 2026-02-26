1c9e445421da5c9043f48356ba62a6c4c1f702ac
Jahnavi2909 <jahnaviboyina929@gmail.com>
2026-02-26 16:30:03 +0530

    Change API base URL to localhost for development

--- src/api/apiCient.js
+++ src/api/apiCient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 export const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "https://d7nbfoifh27pf.cloudfront.net";
+  process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
